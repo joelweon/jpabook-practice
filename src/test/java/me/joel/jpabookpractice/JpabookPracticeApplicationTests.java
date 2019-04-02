c14456906d307afb5c39e34b6b3f330a405cdb7e
joelweon <joelweon@gmail.com>
2019-04-02 19:22:42 +0900

    Initial commit

---
+++ src/test/java/me/joel/jpabookpractice/JpabookPracticeApplicationTests.java
@@ -0,0 +1,16 @@
+package me.joel.jpabookpractice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JpabookPracticeApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
